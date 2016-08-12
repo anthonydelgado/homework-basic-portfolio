ea94073527fb6a7e9c58955bdba4b767fbb6a902
user <user@users-MBP.fios-router.home>
2016-08-12 00:36:56 -0400

    add bio to body copy

--- templates/home.php
+++ templates/home.php
@@ -10,9 +10,15 @@
         <a href="#" class="thumbnail">
             <img src="assets/images/cat.gif" alt="cat">
         </a>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque commodi consectetur cumque dolor dolore eius eos, esse est expedita laudantium libero maxime minima molestias odio optio reprehenderit sapiente suscipit.
+        I'm a software developer & technology evangelist from NYC.
+        I’ve been an explorer of the digital space for over a decade. From helping to design and build custom user interfaces, to creating advanced backend applications that aggregate and collect data, my passion is using software to help change the way people interact with the world around them.
         <br>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque commodi consectetur cumque dolor dolore eius eos, esse est expedita laudantium libero maxime minima molestias odio optio reprehenderit sapiente suscipit.
+        This is a home work assignment that was completed as part of the Rutgers Coding Bootcamp, so please excuse all the Lorem ipsum. If you would like learn more about me, we can
+        <a href="https://www.linkedin.com/in/anthony-delgado">connect on LinkedIn</a>, you can
+        <a href="https://github.com/anthonydelgado">fork me on GitHub</a> or if you really want to get to know me you can
+        <a href="anthonydelgado.github.io">visit my personal portfolio at anthonydelgado.github.io</a>.
+
+
         <br>
         <br>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque commodi consectetur cumque dolor dolore eius eos, esse est expedita laudantium libero maxime minima molestias odio optio reprehenderit sapiente suscipit.
