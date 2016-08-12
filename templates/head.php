8a28ee6cbb34123f7d78e787a08faa963ed2d8a0
user <user@users-MBP.fios-router.home>
2016-08-12 00:43:05 -0400

    add jquery and bs3 js to head

--- templates/head.php
+++ templates/head.php
@@ -6,4 +6,5 @@
 <link rel="stylesheet" href="dist/styles/main.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
